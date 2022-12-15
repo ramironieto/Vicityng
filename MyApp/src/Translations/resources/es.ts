55bdfd1c73d377effdf3f366d723c424b485098f
Ramiro <ramironietopalacio@gmail.com>
2022-12-15 11:27:54 -0300

    init

---
+++ MyApp/src/Translations/resources/es.ts
@@ -0,0 +1,12 @@
+export default {
+  welcome: 'Bienvenido a APP',
+  actions: {
+    continue: 'Continuar',
+  },
+  example: {
+    helloUser: 'Soy un usuario falso {{name}}',
+    labels: {
+      userId: 'Ingresa tu user id',
+    },
+  },
+}
